b4a9c9fb0b13f059b60957953fd36b40c6862699
David Durlan <david.a.durlan@gmail.com>
2020-04-16 10:55:15 +0200

    sliderState works across reloads

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,15 +5,14 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Nav from "./nav"
-// import "./styles.css"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, location }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -23,11 +22,15 @@ const Layout = ({ children }) => {
       }
     }
   `)
+  const [initSlider, setInitSlider] = useState("")
+  useEffect(() => {
+    setInitSlider(location.pathname.split("/")[1])
+  }, [])
 
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <Nav />
+      <Nav initSlider={initSlider} />
       <main>{children}</main>
     </>
   )
--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 const Nav = ({ initSlider }) => {
-  const [sliderState, setSliderState] = useState("slider-portfolio")
+  const [sliderState, setSliderState] = useState("")
+  useEffect(() => {
+    console.log("initSlider", initSlider, initSlider === "")
+
+    initSlider === ""
+      ? setSliderState("slider-portfolio")
+      : setSliderState(`slider-${initSlider}`)
+  }, [initSlider])
   return (
     <nav>
+      <p>{initSlider}</p>
       <div className="navLinks">
         <div className={`slider ${sliderState}`}></div>
 
