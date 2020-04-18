4a609e99450cb67eadc8713f213af29e62cffb26
David Durlan <david.a.durlan@gmail.com>
2020-04-18 15:36:05 +0200

    about me page

--- src/components/about.js
+++ src/components/about.js
@@ -1,18 +1,26 @@
-import React from "react"
-import Image from "./image"
+import React from 'react'
+import Image from './image'
 
 const About = () => {
-  return (
-    <div className="page about">
-      <Image alt="david durlan" filename="personal.jpg" />
-      <h1>Hi, I'm David!</h1>
-      <h4>Frontend Engineer. UX Enthusiast.</h4>
-      <p>
-        I create responsive and intuitive web applications with a dedication to
-        aesthetics, functionality and accessibility
-      </p>
-    </div>
-  )
+    return (
+        <div className="page about">
+            <Image
+                className="david"
+                alt="david durlan"
+                filename="personal.jpg"
+            />
+            <div className="about-text-wrapper">
+                <h1>Hi, I'm David!</h1>
+                <p className="italic subtitle">
+                    Frontend Engineer. UX Enthusiast.
+                </p>
+                <p className="bold">
+                    I create responsive and intuitive web applications with a
+                    dedication to aesthetics, functionality and accessibility.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default About
