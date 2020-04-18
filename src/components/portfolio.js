cc656e1d9446c3cd9e135907a76816310129570a
David Durlan <david.a.durlan@gmail.com>
2020-04-18 19:04:43 +0200

    portfolio navigation mobile

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,23 +1,29 @@
-import React from "react"
-import { Link } from "gatsby"
-import Image from "./image"
+import React from 'react'
+import { Link } from 'gatsby'
+import Image from './image'
 
 const Portfolio = ({ data }) => {
-  return (
-    <div className="page portfolioNav">
-      {data.map((el, id) => {
-        el = el.node.frontmatter
+    return (
+        <div className="page portfolioNav">
+            {data.map((el, id) => {
+                el = el.node.frontmatter
 
-        return (
-          <Link to={`${el.path}`} key={id}>
-            <Image alt={`${el.title}`} filename={el.thumbnail} />
-            <h3>{el.title}</h3>
-            <p>{el.description}</p>
-          </Link>
-        )
-      })}
-    </div>
-  )
+                return (
+                    <Link to={`${el.path}`} key={id}>
+                        <div className="thumnail-text">
+                            <h2>{el.title}</h2>
+                            <p>{el.description}</p>
+                        </div>
+                        <Image
+                            alt={`${el.title}`}
+                            filename={el.thumbnail}
+                            className="thumbnail"
+                        />
+                    </Link>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Portfolio
