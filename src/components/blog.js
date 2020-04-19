fa254e25c80c336d548f70c0e7f780b974bdde98
David Durlan <david.a.durlan@gmail.com>
2020-04-19 13:36:12 +0200

    highlight syntax in markdown posts

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,21 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
 const Blog = ({ data }) => {
-  return (
-    <div className="page blogNav">
-      {data.map((el, id) => {
-        el = el.node.frontmatter
-        return (
-          <Link to={`${el.path}`} key={id}>
-            <h3>{el.title}</h3>
-            <p>{el.date}</p>
-            <p>{el.description}</p>
-          </Link>
-        )
-      })}
-    </div>
-  )
+    return (
+        <div className="page blogNav">
+            {data.map((el, id) => {
+                el = el.node.frontmatter
+                return (
+                    <Link to={`${el.path}`} key={id}>
+                        <h2>{el.title}</h2>
+                        <p className="italic mainColor">{el.date}</p>
+                        <p>{el.description}</p>
+                    </Link>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Blog
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,36 +1,37 @@
-import React from "react"
-import { graphql } from "gatsby"
-import PageTransition from "gatsby-plugin-page-transitions"
+import React from 'react'
+import { graphql } from 'gatsby'
+import PageTransition from 'gatsby-plugin-page-transitions'
 
 export default function blogTemplate({
-  data, // this prop will be injected by the GraphQL query below.
+    data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
-  return (
-    <PageTransition>
-      <div className="page blog-post-container">
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          <p>{frontmatter.date}</p>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </div>
-      </div>
-    </PageTransition>
-  )
+    const { markdownRemark } = data // data.markdownRemark holds your post data
+    const { frontmatter, html } = markdownRemark
+    return (
+        <PageTransition>
+            <div className="page blog-post-container">
+                <div className="blog-post">
+                    <h1>{frontmatter.title}</h1>
+
+                    <p className="italic">{frontmatter.date}</p>
+                    <div
+                        className="blog-post-content"
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                </div>
+            </div>
+        </PageTransition>
+    )
 }
 export const blog = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      frontmatter {
-        path
-        title
-        date
-      }
+    query($path: String!) {
+        markdownRemark(frontmatter: { path: { eq: $path } }) {
+            html
+            frontmatter {
+                path
+                title
+                date
+            }
+        }
     }
-  }
 `
