e76fd4db2f625bee165ba42d29ecdeec9b5e0c2f
David Durlan <david.a.durlan@gmail.com>
2020-04-19 11:59:11 +0200

    redo contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,42 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import autosize from 'autosize'
 
 const Contact = () => {
     const [input, setInput] = useState({})
     const [success, setSuccess] = useState(false)
     const [error, setError] = useState(false)
     const [formInfo, setFormInfo] = useState('')
+    const [animation, setAnimation] = useState(false)
 
-    useEffect(() => {}, [success, error])
-    const handleChange = ({ target }) =>
+    useEffect(() => {
+        autosize(document.querySelector('.contact textarea'))
+    }, [success, error])
+    const handleChange = ({ target }) => {
         setInput({ ...input, [target.name]: target.value })
+    }
 
     const sendEmail = async () => {
-        setError(false)
+        if (!success) {
+            setError(false)
 
-        if (!input.name) {
-            return setFormInfo('* Please provide a name')
-        } else if (!/^.+@.+\..+$/.test(input.email)) {
-            return setFormInfo('* Please provide a valid email')
-        } else if (!input.subject) {
-            return setFormInfo('* Please provide a subject line')
-        } else if (!input.message) {
-            return setFormInfo('* Please provide a message')
-        } else {
-            setFormInfo('Sending...')
-        }
+            if (!input.name) {
+                return setFormInfo('* Please provide a name')
+            } else if (!input.message) {
+                return setFormInfo('* Please provide a message')
+            } else {
+                setFormInfo('Sending...')
+            }
 
-        try {
-            const { data } = await axios.post(
-                'https://2ny34mqbjh.execute-api.eu-central-1.amazonaws.com/prod',
-                input
-            )
-            setSuccess(true)
-            setFormInfo('Success!')
-        } catch (err) {
-            setError(err)
-            setFormInfo(`${err} (most likely David's fault)`)
-            console.log('error while sending an email:', err)
+            try {
+                const { data } = await axios.post(
+                    'https://2ny34mqbjh.execute-api.eu-central-1.amazonaws.com/prod',
+                    input
+                )
+                setSuccess(true)
+                setFormInfo('Success!')
+            } catch (err) {
+                setError(err)
+                setFormInfo(`${err} (most likely David's fault)`)
+                console.log('error while sending an email:', err)
+            }
         }
     }
     const emoji = (() => {
@@ -56,51 +59,47 @@ const Contact = () => {
     })()
     return (
         <div className="page contact">
-            <div className={error ? 'error' : ''}>
-                {formInfo ? (
-                    <p>
-                        {formInfo} {emoji}
-                    </p>
-                ) : (
-                    <p>
-                        I absolutely love comments and feedback, come say hi!
-                        {emoji}
-                    </p>
-                )}
-            </div>
             <div className="contact-form">
-                <div className="upper-form-group">
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="name"
-                        onChange={e => handleChange(e)}
-                    />
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="email"
-                        onChange={e => handleChange(e)}
-                    />
+                <div
+                    className={`formInfo ${error ? 'error' : ''}`}
+                    animation={animation ? '1' : '0'}
+                >
+                    {formInfo ? (
+                        <p>
+                            {formInfo} {emoji}
+                        </p>
+                    ) : (
+                        <p>
+                            I absolutely love comments and feedback, come say
+                            hi!
+                            {emoji}
+                        </p>
+                    )}
                 </div>
-                <div className="lower-form-group">
-                    <input
-                        type="text"
-                        name="subject"
-                        id="subject"
-                        placeholder="subject"
-                        onChange={e => handleChange(e)}
-                    />
-                    <textarea
-                        name="message"
-                        id="message"
-                        placeholder="message"
-                        onChange={e => handleChange(e)}
-                    ></textarea>
-                    <button onClick={sendEmail}>submit</button>
-                </div>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Your name"
+                    onChange={e => handleChange(e)}
+                />
+                <textarea
+                    name="message"
+                    id="message"
+                    placeholder="Message"
+                    onChange={e => {
+                        handleChange(e)
+                    }}
+                    rows="1"
+                ></textarea>
+                <button
+                    onClick={() => {
+                        sendEmail()
+                        setAnimation(!animation)
+                    }}
+                >
+                    Send
+                </button>
             </div>
 
             <div className="socialmedia-wrapper">
